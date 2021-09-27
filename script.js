a034ebfa74ab0d01a7effc8f0d4b07e928ad6867
jpamarcondes <81540117+jpamarcondes@users.noreply.github.com>
2021-09-27 11:38:14 -0300

    Update script.js
    Alteração: img1 para imagem01.

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ botao.addEventListener("click", function () {
     if(botao.value == 'primeiro'){
         nome.innerText = "Isso!! Mas Cuidado pra ele não se apaixonar!"
         situacao.innerText = "Agora sim!!";
-        img1.src="imagem03.png";
+        imagem01.src="imagem03.png";
         botao.value = 'segundo';
     } else if(botao.value == 'segundo'){
         nome.innerText = "Ta gamadinho em você, agora todo cuidado é pouco, ele é bem frágil";
         situacao.innerText = "Gamou em Você!!";
-        img1.src="imagem02.png";
+        imagem01.src="imagem02.png";
         botao.value = 'terceiro';
     } else if(botao.value == 'terceiro'){
         nome.innerText = "Tente alegrar o nosso abacate!";
         situacao.innerText = "Ta Jururu!!";
-        img1.src="imagem01.png";
+        imagem01.src="imagem01.png";
         botao.value = 'primeiro';
     }
 })
